f62ceaa3586852e463ab7ff02271d0ac78b82c4b
Chung10Kr <coohg93@hanmail.net>
2023-07-07 01:29:11 +0900

    Modify Package, Build Option

---
+++ src/main/java/org/securitypass/cors/SecurityPassConst.java
@@ -0,0 +1,15 @@
+package org.securitypass.cors;
+
+/**
+ * fileName       : SecurityPassConst
+ * author         : crlee
+ * date           : 2023/07/04
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/07/04        crlee       최초 생성
+ */
+public class SecurityPassConst {
+    public static final String ALL = "ALL";
+}
